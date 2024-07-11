a5aa603f5b189cf15b1be4498a53e5c639f55cb6
Gabriel Lima <110942377+Gabrielmeia5@users.noreply.github.com>
2024-07-11 11:12:41 -0300

    Project NLW - NodeJS

---
+++ src/lib/dayjs.ts
@@ -0,0 +1,11 @@
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import 'dayjs/locale/pt-br'
+import dayjs from "dayjs";
+
+
+dayjs.locale('pt-br')
+dayjs.extend(localizedFormat)
+
+
+
+export { dayjs }
